6699fe66d1afd6cf272120a55e7b8441056169ac
ally. <79243058+wiredlime@users.noreply.github.com>
2024-05-17 16:09:18 +0700

    fix bugs after accepting friend requests

--- src/components/friend-request-sidebar-option.tsx
+++ src/components/friend-request-sidebar-option.tsx
@@ -32,17 +32,25 @@ function FriendRequestSidebarOption({
     pusherClient.subscribe(
       toPusherKey(`user:${sessionId}:incoming_friend_requests`)
     );
+    pusherClient.subscribe(toPusherKey(`user:${sessionId}:friends`));
 
     const friendRequestHandler = () => {
       setUnseenRequestCount((prev) => prev + 1);
     };
+    const addedFriendHandler = () => {
+      setUnseenRequestCount((prev) => prev - 1);
+    };
     pusherClient.bind(`incoming_friend_requests`, friendRequestHandler);
+    pusherClient.bind(`new_friend`, addedFriendHandler);
 
     return () => {
       pusherClient.unsubscribe(
         toPusherKey(`user:${sessionId}:incoming_friend_requests`)
       );
+      pusherClient.unsubscribe(toPusherKey(`user:${sessionId}:friends`));
+
       pusherClient.unbind(`incoming_friend_requests`, friendRequestHandler);
+      pusherClient.unbind(`new_friend`, addedFriendHandler);
     };
   }, [sessionId]);
 
