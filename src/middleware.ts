810f8bab8af2777d3c23261e29a1bce60099bef7
ally. <79243058+wiredlime@users.noreply.github.com>
2024-05-24 14:15:11 +0700

    update router middleware to skip /dashboard page

--- src/middleware.ts
+++ src/middleware.ts
@@ -29,7 +29,7 @@ export default withAuth(
     }
 
     if (pathname === "/") {
-      return NextResponse.redirect(new URL("/dashboard", req.url));
+      return NextResponse.redirect(new URL("/dashboard/chat", req.url));
     }
   },
   {
