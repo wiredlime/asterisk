260e97089ceb46d86b91ef49a9f64c293091d66c
ally. <79243058+wiredlime@users.noreply.github.com>
2024-05-27 16:10:08 +0700

    add utils to generate gradient background

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -14,3 +14,18 @@ export function chatHrefConstructor(id1: string, id2: string) {
 export function toPusherKey(key: string) {
   return key.replace(/:/g, "__");
 }
+
+const GRADIENT_PALETTE = [
+  "linear-gradient(180deg, #A9C9FF 0%, #FFBBEC 100%)",
+  "linear-gradient(19deg, #FAACA8 0%, #DDD6F3 100%)",
+  "linear-gradient(19deg, #3EECAC 0%, #EE74E1 100%)",
+  "linear-gradient(0deg, #FFDEE9 0%, #B5FFFC 100%)",
+  "linear-gradient(0deg, #D9AFD9 0%, #97D9E1 100%)",
+];
+
+export function randomGradientGenerator() {
+  const randomIndex = Math.floor(
+    Math.random() * (GRADIENT_PALETTE.length - 0) + 0
+  );
+  return GRADIENT_PALETTE[randomIndex];
+}
