71b028b8c80b42c1d3ad6ae7d186e1c404d807d9
ally. <79243058+wiredlime@users.noreply.github.com>
2024-06-03 14:42:13 +0700

    update dashboard page

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,12 +1,10 @@
-import { Asterisk, Send } from "lucide-react";
-import Link from "next/link";
 import React from "react";
 
 const Page = () => {
   return (
     <div className="relative bg-background h-screen w-full grid place-items-center">
       <div
-        className="absolute bottom-36 left-36 w-[40px] h-[40px] dark:blur-sm rounded-full"
+        className="absolute bottom-36 left-36 w-[40px] h-[40px] blur-sm rounded-full"
         style={{
           background:
             "radial-gradient(circle at 18.7% 37.8%, rgb(250, 250, 250) 0%, rgb(225, 234, 238) 90%)",
@@ -14,6 +12,34 @@ const Page = () => {
       ></div>
       <div
         className="absolute top-28 right-28 w-[160px] h-[160px] blur-sm rounded-full"
+        style={{
+          background:
+            "radial-gradient(circle at 18.7% 37.8%, rgb(250, 250, 250) 0%, rgb(225, 234, 238) 90%)",
+        }}
+      ></div>
+      <div
+        className="absolute top-1/2 right-1/3 w-[20px] h-[20px] blur-sm rounded-full"
+        style={{
+          background:
+            "radial-gradient(circle at 18.7% 37.8%, rgb(250, 250, 250) 0%, rgb(225, 234, 238) 90%)",
+        }}
+      ></div>
+      <div
+        className="absolute bottom-56 right-72 w-[100px] h-[100px] blur-sm rounded-full"
+        style={{
+          background:
+            "radial-gradient(circle at 18.7% 37.8%, rgb(250, 250, 250) 0%, rgb(225, 234, 238) 90%)",
+        }}
+      ></div>
+      <div
+        className="absolute top-5 blur-sm left-5 w-[80px] h-[80px] rounded-full"
+        style={{
+          background:
+            "radial-gradient(circle at 18.7% 37.8%, rgb(250, 250, 250) 0%, rgb(225, 234, 238) 90%)",
+        }}
+      ></div>
+      <div
+        className="absolute top-28 left-64 blur-sm w-[30px] h-[30px] rounded-full"
         style={{
           background:
             "radial-gradient(circle at 18.7% 37.8%, rgb(250, 250, 250) 0%, rgb(225, 234, 238) 90%)",
